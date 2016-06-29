135cbf3d79c1a836dc3fe37d9f07186c73353c5d
xiongqushan <45429827@qq.com>
2016-06-29 11:02:46 +0800

    temp submit

---
+++ app/src/main/java/haozuo/com/healthdoctor/view/Activity/CustomDetailActivity.java
@@ -0,0 +1,15 @@
+package haozuo.com.healthdoctor.view.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import haozuo.com.healthdoctor.R;
+
+public class CustomDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_detail);
+    }
+}
